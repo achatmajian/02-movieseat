8e9a080cf87908768ef0b186d37db1407ff5d24d
Arek Chatmajian <arek.chatmajian@gmail.com>
2021-03-23 19:00:49 -0400

    saving data to local storage

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -15,12 +27,14 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Movie select event
 movieSelect.addEventListener("change", e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
-
+// Seat click event
 container.addEventListener("click", e => {
     if (e.target.classList.contains("seat") && 
     !e.target.classList.contains("occupied"))  
